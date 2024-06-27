e84eaf5ad06c14558c5ade79c335491cf9830927
Randraaa <randrahdyn@gmail.com>
2024-06-27 06:36:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var area;
+var circum;
+function square(side){
+   area = side*side;
+   circum = 4*side;
+   console.log('---Square-----')
+   console.log(`Area : ${area} `)
+   console.log(`Circumference : ${circum}`)
+}function rect(panjang , lebar){
+  area = (panjang*lebar);
+  circum = 2(panjang+lebar);
+  console.log('---Rectangle-----')
+  console.log(`Area :  ${area} `)
+  console.log(`Circumference : ${circum}`)
+}
+
---
+++ script1.js
@@ -0,0 +1,15 @@
+function square(side){
+     area = side*side;
+     circum = 4*side;
+     console.log('---Square-----')
+     console.log(`Area : ${area} `)
+     console.log(`Circumference : ${circum}`)
+}function rect(panjang , lebar){
+    area = (panjang*lebar);
+    circum = 2(panjang+lebar);
+    console.log('---Rectangle-----')
+    console.log(`Area :  ${area} `)
+    console.log(`Circumference : ${circum}`)
+}
+var area;
+var circum;
